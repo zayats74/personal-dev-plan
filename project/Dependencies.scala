8c5ea1291311140ef98b698fad5577609074670f
n.zayats <n.zayats74@gmail.com>
2026-03-26 17:18:24 +0400

    feat: add initial project config files

---
+++ project/Dependencies.scala
@@ -0,0 +1,32 @@
+import sbt.*
+
+object Dependencies {
+  val Scala = "3.7.3"
+
+  object cats {
+    private val version       = "2.13.0"
+    private val effectVersion = "3.7-4972921"
+
+    val core         = "org.typelevel" %% "cats-core"          % version
+    val effect       = "org.typelevel" %% "cats-effect"        % effectVersion
+    val effectKernel = "org.typelevel" %% "cats-effect-kernel" % effectVersion
+  }
+
+  object weaver {
+    private val version = "0.8.4"
+
+    val cats = "com.disneystreaming" %% "weaver-cats" % version
+  }
+
+  val pdpLibs: Seq[ModuleID] =
+    Seq(
+      cats.core,
+      cats.effect,
+      cats.effectKernel
+    )
+
+  val testLibs: Seq[ModuleID] =
+    Seq(
+      weaver.cats
+    ).map(_ % Test)
+}
